370262de8898dd5f7cc96d4f43b2aa3ed58b9e25
pHedi3 <federico.gualdi3@gmail.com>
2021-06-03 18:05:50 +0200

    funziona ache sbagliando cognome in minuscolo

--- js/script.js
+++ js/script.js
@@ -4,12 +4,30 @@ var outputTwo = outputDiv[1];
 var listName = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
 var subNameUser = prompt('inserisci il tuo cognome');
 listName.push(subNameUser)
-listName.sort();
+
+
+var listNameLow = [];
+var x = 0;
+while (listName.length > listNameLow.length) {
+    listNameLow.push(listName[x].toLowerCase());
+    x++;
+}
+
+listNameLow.sort();
+
+
+var listNameRight =[];
+var y = 0;
+while (y < listNameLow.length) {
+   listNameRight.push(listNameLow[y].charAt(0).toUpperCase() + listNameLow[y].slice(1));
+    y++;
+}
+
 var text = '';
 var i = 0;
-while (i < listName.length) {
-    text +='<li>' + listName[i] + '</li>';
+while (i < listNameRight.length) {
+    text +='<li>' + listNameRight[i] + '</li>';
     i++;
 }
 outputOne.innerHTML = text;
-outputTwo.innerHTML += 'ti trovi in ordine alfabetico ' + (listName.indexOf(subNameUser) + 1 ) ;
+outputTwo.innerHTML = 'ti trovi in ordine alfabetico ' + (listNameRight.indexOf(subNameUser.charAt(0).toUpperCase() + subNameUser.slice(1)) + 1 ) ;
