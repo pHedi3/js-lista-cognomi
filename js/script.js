e9315838859274d3def7e9bdd74f3fd1ff744e5d
pHedi3 <federico.gualdi3@gmail.com>
2021-06-03 15:17:39 +0200

    fine es

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-var outputDiv = document.getElementsByClassName('result')
-var outputOne = outputDiv[0]
-var listName = ['']
-
-outputOne.innerHTML = 'ciao';
+var outputDiv = document.getElementsByClassName('result');
+var outputOne = outputDiv[0];
+var listName = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+var subNameUser = prompt('inserisci il tuo cognome');
+listName.push(subNameUser)
+listName.sort();
+var text = '';
+var i = 0;
+while (i < listName.length) {
+    text += listName[i] + '<br>'
+    i++
+}
+outputOne.innerHTML = text;
+outputOne.innerHTML += 'ti trovi in ordine alfabetico ' + (listName.indexOf(subNameUser) + 1 ) ;
