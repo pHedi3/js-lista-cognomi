c1f9d67079726b946bf3b315fcfed5650917aad6
pHedi3 <federico.gualdi3@gmail.com>
2021-06-03 17:18:39 +0200

    corezzione con ol

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var outputDiv = document.getElementsByClassName('result');
 var outputOne = outputDiv[0];
+var outputTwo = outputDiv[1];
 var listName = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
 var subNameUser = prompt('inserisci il tuo cognome');
 listName.push(subNameUser)
@@ -7,8 +8,8 @@ listName.sort();
 var text = '';
 var i = 0;
 while (i < listName.length) {
-    text += listName[i] + '<br>'
-    i++
+    text +='<li>' + listName[i] + '</li>';
+    i++;
 }
 outputOne.innerHTML = text;
-outputOne.innerHTML += 'ti trovi in ordine alfabetico ' + (listName.indexOf(subNameUser) + 1 ) ;
+outputTwo.innerHTML += 'ti trovi in ordine alfabetico ' + (listName.indexOf(subNameUser) + 1 ) ;
