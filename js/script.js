5c016f0fa2d3d116db294a2c17f4b48598e7c74a
pHedi3 <federico.gualdi3@gmail.com>
2021-06-04 15:55:44 +0200

    versione più semplice

--- js/script.js
+++ js/script.js
@@ -3,31 +3,15 @@ var outputOne = outputDiv[0];
 var outputTwo = outputDiv[1];
 var listName = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
 var subNameUser = prompt('inserisci il tuo cognome');
+subNameUser = subNameUser[0].toUpperCase() + subNameUser.slice(1)
 listName.push(subNameUser)
-
-
-var listNameLow = [];
-var x = 0;
-while (listName.length > listNameLow.length) {
-    listNameLow.push(listName[x].toLowerCase());
-    x++;
-}
-
-listNameLow.sort();
-
-
-var listNameRight =[];
-var y = 0;
-while (y < listNameLow.length) {
-   listNameRight.push(listNameLow[y].charAt(0).toUpperCase() + listNameLow[y].slice(1));
-    y++;
-}
+listName.sort();
 
 var text = '';
 var i = 0;
-while (i < listNameRight.length) {
-    text +='<li>' + listNameRight[i] + '</li>';
+while (i < listName.length) {
+    text +='<li>' + listName[i] + '</li>';
     i++;
 }
 outputOne.innerHTML = text;
-outputTwo.innerHTML = 'ti trovi in ordine alfabetico ' + (listNameRight.indexOf(subNameUser.charAt(0).toUpperCase() + subNameUser.slice(1)) + 1 ) ;
+outputTwo.innerHTML = 'ti trovi in ordine alfabetico ' + (listName.indexOf(subNameUser) + 1 ) ;
